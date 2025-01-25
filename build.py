b6357c8c4a433f3ec2a0510e94e201d4e113092a
joelefrain <etalarconjoel@gmail.com>
2025-01-25 14:02:55 -0500

    add. get_picked_events | update. get_displacement | optimize. site_response by parallelization

--- build.py
+++ build.py
@@ -1,2 +1,8 @@
 # 1. Function that reads DB and writes CSV files for each geotechincal column for geotechnical structure.
 # The path must be ./data/site_response/{company}/{project}/{geotechnical_structure}/{geotechnical_column}.csv
+
+# 2. Function that reads XML files for the inventory of each sensor and writes XML.
+# The path must be ./data/processor/inventory/{network}.{station}.{location}.xml
+
+# 3. Function that creates a symbolic link for mseed archive.
+# The path must be ./data/processor/events/{year}/{network}/{station}/{model}{component}.D/{network}.{station}.{location}.{model}{component}.D.{year}.{day_number}
